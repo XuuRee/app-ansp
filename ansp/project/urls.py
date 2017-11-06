8f7c4f9c39c8b77b8a39a6d7b5c75ec7d9c9e07c
Filip Valchář <Filip Valch>
2017-11-06 17:52:55 +0100

    project index, project detail

--- ansp/project/urls.py
+++ ansp/project/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),     # index function
+    url(r'^$', views.index, name='index'),                              # /projects/
+    url(r'^(?P<id_project>[0-9]+)/$', views.detail, name="detail"),     # /projects/<id_project>
 ]
--- ansp/project/views.py
+++ ansp/project/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.http import HttpResponse
 from django.utils import timezone
 from .models import Project, File
 
 # Create your views here.
 
+
 def index(request):
-    projects, files = Project.objects.all(), File.objects.all()
-    return render(request, 'project/index.html', {'projects' : projects, 'files' : files})
+    all_projects = Project.objects.all()
+    return render(request, "project/index.html/", { "all_projects": all_projects })
+    
+   
+def detail(request, id_project):
+    try:
+        specific_project = Project.objects.get(pk=id_project)
+    except Project.DoesNotExist:
+        raise Http404("Project does not exist")
+    files = File.objects.filter(id_project=id_project)
+    context = {
+        "specific_project": specific_project,
+        "files": files,
+    }
+    return render(request, "project/detail.html", context)
+    
+    
