d7d622aa00525df6d499b1690d018d4e7fb530f5
Filip Valchář <Filip Valch>
2017-11-25 18:04:50 +0100

    repair generic views

--- ansp/project/urls.py
+++ ansp/project/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from . import views
+from .import views
 
 app_name = 'project'
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),                      # /projects/
+    url(r'^$', views.IndexView.as_view(), name='index'),                              # /projects/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),     # /projects/<id_project>
 ]
--- ansp/project/views.py
+++ ansp/project/views.py
@@ -1,12 +1,13 @@
 #from django.shortcuts import render, get_object_or_404
 #from django.utils import timezone
+from .models import Project
 from django.views import generic
-from .models import Project, File
+
 
 # Create your views here.
+# Generic views IndexView(), DetailView()
 
 class IndexView(generic.ListView):
-    
     template_name = 'project/index.html'
     context_object_name = 'all_projects'
 
@@ -15,6 +16,7 @@ class IndexView(generic.ListView):
 
 
 class DetailView(generic.DetailView):
-    
     model = Project
+    context_object_name = 'specific_project'
     template_name = 'project/detail.html'
+    
