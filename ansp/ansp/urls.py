5ac3c4f8306911b7900627eff80ca588b302e0a2
Filip Valchář <Filip Valch>
2017-12-12 16:01:09 +0100

    start app accounts

--- ansp/ansp/urls.py
+++ ansp/ansp/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^projects/', include('project.urls')),
+    url(r'^accounts/', include('account.urls')),
 ]
 
 if settings.DEBUG:
