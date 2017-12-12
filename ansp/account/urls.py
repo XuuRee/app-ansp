d002a6a27d08b7e9bd144b6c8fc8fed05cfa470b
XuuRee <filipvalchar@gmail.com>
2017-12-12 17:11:07 +0100

    login html

--- ansp/account/urls.py
+++ ansp/account/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login
 from . import views
 
+app_name = 'account'
+
 urlpatterns = [
-    url(r'^$', views.home)
+    # /account/ ... login, register etc. 
+    url(r'^$', views.home),
+    # /account/login
+    url(r'^login/$', login, {'template_name': 'account/login.html'})
 ]
