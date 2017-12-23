2fdbcbdb17cb94ff43143b512ff9d64ac682c3f1
Filip Valchář <xvalchar@fi.muni.cz>
2017-12-23 19:55:51 +0100

    migrations on models, finish attr

--- ansp/project/models.py
+++ ansp/project/models.py
@@ -5,13 +5,13 @@ from django.core.urlresolvers import reverse
 
 
 class Project(models.Model):
-    id_project = models.AutoField(primary_key=True)     # delete item
-    author = models.ForeignKey('auth.User')             # delete item
+    id_project = models.AutoField(primary_key=True)
+    author = models.ForeignKey('auth.User')
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     created = models.DateField(_("Created"), default=datetime.date.today)
-    deadline = models.DateField()
-    finished = models.DateField(null=True, blank=True)  # True/False
+    deadline = models.DateField(null=True, blank=True)
+    finish = models.BooleanField(default=True)
 
     def get_absolute_url(self):
         return reverse('project:detail', kwargs={'pk': self.id_project})
@@ -24,7 +24,7 @@ class File(models.Model):
     id_file = models.AutoField(primary_key=True)        # delete item
     id_project = models.ForeignKey('Project', on_delete=models.CASCADE)
     filename = models.CharField(max_length=100)
-    filepath = models.FileField(null=True, blank=True)  # cant be null
+    filepath = models.FileField(null=True, blank=False)
 
     def __str__(self):
         return self.filename
